19a07fe733660346668f31dfe10aff98551068ee
Gerardo Garza <owlscloud@gmail.com>
2022-06-27 15:17:21 -0500

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "../styles/contact.scss";
+import andrea from "../assets/images/andrea_contact.PNG";
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="contact">
+      <div className="contact__content">
+        <div></div>
+        <img src={andrea} alt="" />
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
