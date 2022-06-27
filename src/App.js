14c70742d5be64f07b8a630f1a871ea494143255
Gerardo Garza <owlscloud@gmail.com>
2022-06-27 08:18:01 -0500

    aadded winds page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
+import UTRGVwinds from "./pages/UTRGVwinds";
+import SocialMedia from "./pages/SocialMedia";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <>
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Portfolio />} />
         <Route path="/about" element={<About />} />
+        <Route path="/winds" element={<UTRGVwinds />} />
+        <Route path="/social" element={<SocialMedia />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
---
+++ src/pages/UTRGVwinds.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "../styles/winds.scss";
+function UTRGVwinds() {
+  return (
+    <section className="winds">
+      <h1 className="winds__title">UTRGV WINDS</h1>
+      <div className="winds__content">
+        <p className="winds__content-first">
+          April 14th, 2019. We competed at the WGI World Championships for the
+          first time ever this weekend, as a first year group, from a small
+          region called THE RIO GRANDE VALLEY.
+          <br />
+          <br /> UTRGV Winds, the group that set the standard. The Rio Grande
+          Valley is a place we all call home. A place where we find ourselves
+          proud of our culture and where we show nothing but hard work and
+          dedication. Slowly growing and showing the world what we are capable
+          of.
+          <br />
+          <br /> When we first became an organization our goal for this year was
+          to receive a medal in open class. Well folks, we exceeded that goal by
+          getting bumped up to world-class, competing among the toughest groups
+          in the world, and receiving 3rd place! We are beyond proud of everyone
+          and we can not thank you enough for your support.
+          <br />
+          <br /> We have had the blessed opportunity to have such motivated and
+          driven students and staff to make this happen. Dr. Saul Torres, Mr.
+          Dempsey, Ben Lee, Edgar Ortiz, Ediel Ovalle, Tirzah Claus, and so many
+          more people on board were the foundation of what this group needed to
+          be pushed forward. Along with the help of our local high schools
+          McAllen HS, and PSJA Southwest High School, a big thank you for coming
+          together and lending us equipment to use to for the season.
+          <br />
+          <br /> ​ Until next season. Stay on the lookout for audition
+          information
+        </p>
+        <hr />
+        <p>
+          Saturday, April 23rd. We competed at the WGI World Championships for
+          the SECOND time ever this weekend.
+          <br />
+          <br />
+          ​ After a 26-hour bus ride, the group heads straight to sleep and is
+          up bright and early to rehearse for prelims. Spending over a day on
+          the bus, the group is tired but ready to do what they have to do. Up
+          and early at 5 am. With a long day ahead of them the energy, the group
+          is showing is unconditional. The love and support being sent from back
+          home is overwhelming and has the members enthralled and ready to
+          compete. <br />
+          <br />
+          ​ UTRGV Winds comes out second in prelims with a 92.850. Right behind
+          Stryke with a 92.850.
+          <br />
+          <br />
+          ​ Back to work.
+          <br />
+          <br />
+          ​ The group heads back to the hotel and gets ready to rest up for the
+          final day. <br />
+          <br />
+          Last-minute preparations, paintings, students washing their uniforms
+          and getting everything ready for the big day. Members are sharing
+          moments from the season as they stare at the night sky. Moments they
+          loved, moments they loved not as much, and moments they have been
+          wanting to share. No matter the results, the group knew they made it.
+          The past three years were overwhelming and never knew what was to
+          come. After having their second season cut short due to COVID-19, They
+          were there. <br />
+          <br />
+          Sunday, April 24th, 2022.
+          <br />
+          <br />
+          UTRGV Winds. 95.325.
+          <br />
+          <br />
+          ​ Tears. Everywhere. ​<br />
+          <br />
+          This group, from way down south. ​<br />
+          <br />
+          The UTRGV Winds come home with First place. For the first time in the
+          group's history, Their second time at WGI Championships. Making
+          history a second time in a row.
+          <br />
+          <br />
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default UTRGVwinds;
